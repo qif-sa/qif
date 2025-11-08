3c42c234dde22195e7cbc0fbcea7872d35ab811c
qif-sa <hpadxa745@gmail.com>
2025-11-08 14:31:10 +0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,27 @@
+const socialLinks = [
+    {
+        name: "Menu",
+        url: "https://qif-sa.github.io/menu/",
+        iconClass: "fas fa-mug-hot" 
+    },
+    {
+        name: "TikTok",
+        url: "https://www.tiktok.com/@qif_sa",
+        iconClass: "fab fa-tiktok"
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/qif_sa",
+        iconClass: "fab fa-instagram"
+    },
+    {
+        name: "Snapchat",
+        url: "https://snapchat.com/t/9efXXNWu",
+        iconClass: "fab fa-snapchat-ghost"
+    },
+    {
+        name: "Google Map",
+        url: "https://www.google.com/maps",
+        iconClass: "fas fa-map-marker-alt"
+    }
+];
